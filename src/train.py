ecd036305c74d009d52b86b17d57e9df5aed3d47
Mathias Vigouroux <56303941+MathiasVigouroux@users.noreply.github.com>
2024-03-02 21:34:05 +0100

    Trying to add an actor critic model

--- src/train.py
+++ src/train.py
@@ -1,5 +1,11 @@
 from gymnasium.wrappers import TimeLimit
 from env_hiv import HIVPatient
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.distributions import Categorical
+import numpy as np
+
 
 env = TimeLimit(
     env=HIVPatient(domain_randomization=False), max_episode_steps=200
@@ -10,12 +16,74 @@ env = TimeLimit(
 # You have to implement your own agent.
 # Don't modify the methods names and signatures, but you can add methods.
 # ENJOY!
+#class ProjectAgent:
+#    def act(self, observation, use_random=False):
+#        return 0
+
+#    def save(self, path):
+#        pass
+
+#    def load(self):
+#        pass
+
+
+class ActorCritic(nn.Module):
+    def __init__(self, state_space, action_space, hidden_size=256):
+        super(ActorCritic, self).__init__()
+        self.affine = nn.Linear(state_space, hidden_size)
+        
+        # Actor head
+        self.action_layer = nn.Linear(hidden_size, action_space)
+        
+        # Critic head
+        self.value_layer = nn.Linear(hidden_size, 1)
+        
+    def forward(self, state):
+        state = torch.relu(self.affine(state))
+        
+        action_probs = torch.softmax(self.action_layer(state), dim=-1)
+        state_values = self.value_layer(state)
+        
+        return action_probs, state_values
+
 class ProjectAgent:
-    def act(self, observation, use_random=False):
-        return 0
-
+    def __init__(self, state_space, action_space, lr=3e-4, gamma=0.99):
+        self.model = ActorCritic(state_space, action_space)
+        self.optimizer = optim.Adam(self.model.parameters(), lr=lr)
+        self.gamma = gamma
+        
+    def act(self, state):
+        state = torch.from_numpy(state).float().unsqueeze(0)
+        action_probs, _ = self.model(state)
+        m = Categorical(action_probs)
+        action = m.sample()
+        return action.item()
+    
+    def update(self, rewards, log_probs, state_values, next_state_value):
+        # Calculate discounted rewards
+        returns = []
+        R = next_state_value
+        for step in reversed(range(len(rewards))):
+            R = rewards[step] + self.gamma * R
+            returns.insert(0, R)
+            
+        returns = torch.tensor(returns)
+        log_probs = torch.stack(log_probs)
+        state_values = torch.stack(state_values)
+        
+        advantage = returns - state_values
+        
+        actor_loss = -(log_probs * advantage.detach()).mean()
+        critic_loss = advantage.pow(2).mean()
+        
+        self.optimizer.zero_grad()
+        (actor_loss + critic_loss).backward()
+        self.optimizer.step()
+    
     def save(self, path):
-        pass
+        torch.save(self.model.state_dict(), path)
+    
+    def load(self, path):
+        self.model.load_state_dict(torch.load(path))
+        self.model.eval()
 
-    def load(self):
-        pass
